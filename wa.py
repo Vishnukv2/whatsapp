574345b5c70dc8f962108d4f39112d475d2bda76
Ani14-ai <138982309+Ani14-ai@users.noreply.github.com>
2024-06-05 13:47:45 +0530

    Update wa.py

--- wa.py
+++ wa.py
@@ -280,8 +280,7 @@ import subprocess
 import threading
 def run_ngrok():
     try:
-        while True:
-            subprocess.run(["ngrok", "http", "--domain=intent-sharply-kodiak.ngrok-free.app", "8000"], check=True)
+        subprocess.run(["ngrok", "http", "--domain=intent-sharply-kodiak.ngrok-free.app", "8000"], check=True)
     except subprocess.CalledProcessError as e:
         print(f"An error occurred: {e}")
         # Handle the error, if needed
