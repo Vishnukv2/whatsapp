4a1a671c4408fbb73db6d6ab2f1a3e6a0b36c1ab
Ani14-ai <138982309+Ani14-ai@users.noreply.github.com>
2024-06-11 14:29:41 +0530

    Update wa.py

--- wa.py
+++ wa.py
@@ -61,7 +61,7 @@ def send_whatsapp_message():
                 "recipient_type": "individual",
                 "to": recipient,
                 "type": "template",
-                "template": {"name": "welcome", "language": {"code": "en"}},
+                "template": {"name": "intellect", "language": {"code": "ar"}},
             })
 
             loop = asyncio.new_event_loop()
