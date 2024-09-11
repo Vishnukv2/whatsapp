0561b95baba4d8cdef9df0c6f50aaaecaee5ee31
Ani14-ai <138982309+Ani14-ai@users.noreply.github.com>
2024-09-11 20:48:44 +0530

    Update wa.py

--- wa.py
+++ wa.py
@@ -435,8 +435,7 @@ def insert_chat_history(user_input, bot_response, phone_number):
             INSERT INTO tbWhatsAppChat (ClientID, User_input, Bot_response)
             VALUES (?, ?, ?, ?)
         """
-        cursor.execute(insert_chat_query, (client_id, user_input, bot_response)
-        
+        cursor.execute(insert_chat_query, (client_id, user_input, bot_response))        
         connection.commit()
         connection.close()
 
